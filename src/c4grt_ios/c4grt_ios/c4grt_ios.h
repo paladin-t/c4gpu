6cb70c44a22dfbb8601ba7d0ea35832cd622df8c
Wang Renxin <hellotony521@qq.com>
2017-02-27 16:20:13 +0800

    +added xcode projects.

---
+++ src/c4grt_ios/c4grt_ios/c4grt_ios.h
@@ -0,0 +1,4 @@
+#ifndef __C4GRT_IOS_H__
+#define __C4GRT_IOS_H__
+
+#endif /* __C4GRT_IOS_H__ */
---
+++ src/c4grt_macos/c4grt_macos/c4grt_macos.h
@@ -0,0 +1,4 @@
+#ifndef __C4GRT_MACOS_H__
+#define __C4GRT_MACOS_H__
+
+#endif /* __C4GRT_MACOS_H__ */
